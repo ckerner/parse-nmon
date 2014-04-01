79bd5b1e30aa4cc9493cbda185be25c36abe1f18
Chad Kerner <ckerner@illinois.edu>
2014-04-01 12:34:47 -0500

    Added processing of CPU_ALL nmon records.

---
+++ gnuplot-CPU_ALL.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+PNGFILE=$1
+MYHOST=$2
+MYDATE=$3
+INFILE=$4
+
+GNUPLOT=`which gnuplot`
+
+if [ "x${INFILE}" == "x" ] ; then
+   LOGFILE="nmon.out"
+else
+   LOGFILE="${INFILE}"
+fi
+
+
+${GNUPLOT} <<EOF
+set timefmt "%Y-%m-%d:%H:%M"
+set xlabel "Time Interval (Minutes)"
+set yrange [0:]
+set xrange [0:1450]
+set ylabel "CPU Usage(Percent)"
+set title "CPU Utilization - ${MYHOST} - ${MYDATE}"
+set grid
+set datafile separator ","
+set key left box
+set terminal dumb
+set xtics out nomirror
+plot "${LOGFILE}" using 2 title 'User %' with lines, \
+     "${LOGFILE}" using 3 title 'System %' with lines, \
+     "${LOGFILE}" using 4 title 'Wait %' with lines, \
+     "${LOGFILE}" using 5 title 'Idle %' with lines 
+set terminal png size 600,400
+set output "${PNGFILE}"
+replot
+EOF
+
+
